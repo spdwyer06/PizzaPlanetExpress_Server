d8c2075a08b6884ec6db1737510fbe1b13340754
Sean Dwyer <spdwyer06@gmail.com>
2020-12-02 21:53:36 -0500

    Created middleware

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const Express = require('express');
 const app = Express();
 const db = require('./db');
 
+
 db.sync();
 app.use(Express.json());
+
+
+app.use('/user', require('./Controllers/userController'));
 
 
 app.listen(process.env.PORT, () => console.log(`Server is running on Port ${process.env.PORT}`));
