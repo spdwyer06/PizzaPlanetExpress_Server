a77d1664f2a617442b54e4ffbde123b1a95ccb45
Sean Dwyer <spdwyer06@gmail.com>
2021-01-30 17:28:12 -0500

    Fixed?

--- db.js
+++ db.js
@@ -9,9 +9,7 @@ const Sequelize = require('sequelize');
 //     // }
 // });
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
-});
+const db = new Sequelize(process.env.DATABASE_URL);
 
 db.authenticate()
     .then(() => console.log('Postgres db connected'))
